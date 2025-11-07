78e12da9339cd97dc08442af7fe5a0452f398cd1
Prachita <prachita@Prachitas-MacBook-Air.local>
2025-11-06 19:07:15 -0500

    2

--- app/Labs/layout.tsx
+++ app/Labs/layout.tsx
@@ -1,19 +1,21 @@
+export const dynamic = "force-dynamic"; // 🚀 prevents Vercel prerender crash
+
 import { ReactNode } from "react";
 import TOC from "./TOC";
-// import "./globals.css";
 
 export default function LabsLayout({
- children,
+  children,
 }: Readonly<{ children: ReactNode }>) {
- return (
-   <table>
-     <tbody>
-       <tr>
-         <td valign="top" width="100px">
-           <TOC />
-         </td>
-         <td valign="top">{children}</td>
-       </tr>
-     </tbody>
-   </table>
-);}
+  return (
+    <table style={{ width: "100%" }}>
+      <tbody>
+        <tr>
+          <td valign="top" width="100px">
+            <TOC />
+          </td>
+          <td valign="top">{children}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
